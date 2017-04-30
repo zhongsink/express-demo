3268c218f59bb8a81bf0251db60be5876b552c99
zelsx <zhongink@gmail.com>
2017-04-30 11:04:33 +0800

    fix cannot setheader error

--- routes/index.js
+++ routes/index.js
@@ -3,14 +3,11 @@ const fs = require("fs");
 const express = require('express');
 const router = express.Router();
 const db = require("../model/db.js");
-const crypto = require("crypto");
+const md5 = require("../model/md5.js");
 const Article = require("../model/article.js");
 const Project = require("../model/project.js");
 const User = require("../model/user.js");
-const formidable = require('formidable');
 
-const form = new formidable.IncomingForm();
-const md5 = crypto.createHash('md5');
 /* GET home page. */
 router.get('/', function (req, res) {
     res.header("Content-Type", "application/html;charset=utf-8");
@@ -25,120 +22,124 @@ router.get('/getItems', function (req, res) {
     });
 });
 router.post('/register', function (req, res) {
-    form.parse(req, function (err, fields, files) {
-        // console.log(fields);    //打印为空对象
-        User.findOne({ user: fields.username }, (err, result) => {
-            if (err || result) {
-                res.json(JSON.stringify({ success: false }));
-                if (err) return;
-            }
-            else {
-                User.create({
-                    user: fields.username,
-                    password: md5.update(fields.password).digest('hex'),
-                }, (err) => {
-                    if (err) res.json(JSON.stringify({ success: false }));
-                    res.json(JSON.stringify({ success: true }));
-                });
-            }
-        })
-        // res.json(JSON.stringify(fields));
+    let fields = req.fields;
+    User.findOne({ user: fields.user }, (err, result) => {
+        if (err || result) {
+            // console.log(1)
+              res.json(JSON.stringify({ success: false }));
+              return;
+        }
+        else {
+            User.create({
+                user: fields.user,
+                password: md5(fields.password),
+            }, (err) => {
+                if (err) {
+                    // console.log(2);
+                     res.json(JSON.stringify({ success: false }));
+                     return;
+                }
+                // console.log(3);
+                 return res.json(JSON.stringify({ success: true }));
+            });
+        }
     });
+
 });
 router.post('/signin', function (req, res) {
-    form.parse(req, function (err, fields, files) {
-        User.findOne({ user: fields.username }, (err, result) => {
-            console.log(result);
-            if (err) {
-                res.json(JSON.stringify({ success: false }));
-                return;
-            }
-            if (md5.update(fields.password).digest('hex') === result.password)
-                res.json(JSON.stringify({ success: true }));
-            else res.json(JSON.stringify({ success: false }));
-        })
+    let fields = req.fields;
+    let password = md5(fields.password);
+    User.findOne({ user: fields.user }, (err, result) => {
+        // console.log(result);
+        if (err || !result) {
+            console.log(1);
+            res.json(JSON.stringify({ success: false }));
+            return;
+        }
+        if (password === result.password) {
+            console.log(2)
+            return res.json(JSON.stringify({ success: true }));
+        }
+        else {
+            console.log(3)
+            return res.json(JSON.stringify({ success: false }));
+        }
     });
+
 });
 
 router.post('/insertItems', function (req, res) {
 
-    form.parse(req, function (err, fields, files) {
-        // console.log(fields);
-        Article.findOne({
-            hash: fields.hash,
-            title: fields.title
-        }, (err, result) => {
-            // console.log(result);
-            if (err || result) {
-                res.json({ "success": false });
+    let fields = req.fields;
+    Article.findOne({
+        hash: fields.hash,
+        title: fields.title
+    }, (err, result) => {
+        // console.log(result);
+        if (err || result) {
+            return res.json({ "success": false });
+        }
+        else {
+            Article.create(fields, function (err) {
                 if (err) {
+                    // console.log(err);
+                    res.json({ "success": false });
                     return;
                 }
-            }
-            else {
-                Article.create(fields, function (err) {
-                    if (err) {
-                        // console.log(err);
-                        res.json({ "success": false });
-                        return;
-                    }
-                    else {
-                        res.json({ "success": true });
-                    }
-                });
-            }
-        });
+                else {
+                    return res.json({ "success": true });
+                }
+            });
+        }
     });
 });
 
 router.post('/insertProjectItems', (req, res) => {
 
-    form.uploadDir = path.join(__dirname + "/../public/images");
-    form.parse(req, function (err, fields, files) {
-
-        Project.findOne({
-            title: fields.title
-        }, (err, result) => {
-            if (err || result) {
-                fs.unlink(files.img.path, (err) => {
-                    if (err) {
-                        res.json({ "success": false });
-                        return;
-                    }
-                });
+    let fields = req.fields;
+    let files = req.files
+    Project.findOne({
+        title: fields.title
+    }, (err, result) => {
+        if (err || result) {
+            fs.unlink(files.img.path, (err) => {
                 if (err) {
                     res.json({ "success": false });
                     return;
                 }
-                else if (result)
+            });
+            if (err) {
+                res.json({ "success": false });
+                return;
+            }
+            else if (result)
+                return res.json({ "success": false });
+        } else {
+            // console.log(fields, files);
+            fs.rename(files.img.path, path.join(__dirname + "/../public/images/") + files.img.name, (err) => {
+                if (err) {
                     res.json({ "success": false });
-            } else {
-                // console.log(fields, files);
-                fs.rename(files.img.path, path.join(__dirname + "/../public/images/") + files.img.name, (err) => {
+                    return;
+                }
+                Project.create({
+                    user: fields.user,
+                    url: fields.url,
+                    ImgUrl: "http://120.25.221.52" + "/images/" + files.img.name,
+                    title: fields.title,
+                    githubURL: fields.githubUrl,
+                    tags: [fields.tags],
+                    createAt: fields.createAt
+                }, function (err) {
                     if (err) {
                         res.json({ "success": false });
                         return;
                     }
-                    Project.create({
-                        user: fields.user,
-                        url: fields.url,
-                        ImgUrl: "http://120.25.221.52" + "/images/" + files.img.name,
-                        title: fields.title,
-                        githubURL: fields.githubUrl,
-                        tags: [fields.tags],
-                        createAt: fields.createAt
-                    }, function (err) {
-                        if (err) {
-                            res.json({ "success": false });
-                            return;
-                        }
-                        else {
-                            res.json({ "success": true });
-                        }
-                    });
+                    else {
+                        return res.json({ "success": true });
+                    }
                 });
-            }
-        });
+            });
+        }
     });
 });
 
@@ -150,5 +151,6 @@ router.get('/getProjectItem', function (req, res) {
     });
 
 });
+
 
 module.exports = router;
